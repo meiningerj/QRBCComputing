8f47756ca95187885fcb0faabd31d554dda0c138
Jamie Meininger <j.p.meininger@se15.qmul.ac.uk>
2018-10-04 18:50:42 +0100

    adding tasks
    adding tasks

---
+++ Level 1/Level1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+from ..KeyMaker import *
+
+if len(sys.argv) != 2:
+	print "Incorrect number of command line arguments."
+    print "Usage:"
+    print "    python ./numbers.py <arg1> <arg2>"
+    exit(1)
+
+var1 = int(sys.argv[1])
+
+if var1 = 4:
+	print "correct the key is" + makekey("Level1")
